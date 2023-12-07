4c9c65ebf7170bf67dd9ddee3755c8cd2e29ef49
Vladimir Borovikov <91381325+iamvldmrbrvkv@users.noreply.github.com>
2023-12-07 21:59:22 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+$(document).ready(() => {
+  $('.menu').on('mouseenter', () => {
+    $('.nav-menu').show()
+  }).on('mouseleave', () => {
+    $('.nav-menu').hide()
+  })
+
+  $('.btn').on('mouseenter', event => {
+    $(event.currentTarget).addClass('btn-hover')
+  }).on('mouseleave', event => {
+    $(event.currentTarget).removeClass('btn-hover')
+  })
+
+  $('.postText').on('keyup', event => {
+    const post = $(event.currentTarget).val()
+    const remaining = 140 - post.length
+    if (remaining <= 0) {
+      $('.wordcount').addClass('red')
+    } else {
+      $('.wordcount').removeClass('red')
+    }
+    $('.characters').html(remaining)
+  })
+
+  $('.postText').focus()
+}); 
